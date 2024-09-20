8d7672ba9a5d4fabcf6ddd6ed0523c2f55956722
odd_zeal_14 <subhagopal333@gmail.com>
2024-09-20 20:00:03 +0530

    largest_element_in_array_gfg

---
+++ GFG/Basic/Largest_Element_in_Array_I.py
@@ -0,0 +1,15 @@
+print("LARGEST ELEMENT IN ARRAY I")
+
+arr=[2,5,1,3,0]
+
+max = arr[0]
+for i in range (0,len(arr)):
+    if max < arr[i]:
+        max = arr[i]
+
+print(max)
+        
+
+# --------------------------------------------
+# 5
+# --------------------------------------------
